e886e2ca4a299c3b9d0c30a778be7d01aebbd4fa
Nick Ludlam <nick@recoil.org>
2022-12-15 22:39:53 +0000

    Added ability to pass in an existing route. Changed some of the default simulation starting points

--- speed_compare/rust_speed_test/src/main.rs
+++ speed_compare/rust_speed_test/src/main.rs
@@ -26,9 +26,11 @@ fn main() {
     let start = Instant::now();
 
     let mut simulation_run_count = 2000;
+    let mut seed_route: Vec<usize> = Vec::new();
 
     // Get some args
     let args: Vec<String> = env::args().collect();
+    println!("args.len={}", args.len());
     match args.len() {
         // no arguments passed
         1 => {
@@ -53,6 +55,31 @@ fn main() {
             simulation_run_count = number;
             println!("Running with simulation count of {}", simulation_run_count);
         }
+        3 => {
+            let simulation_run_count_arg = &args[1];
+            // parse the number
+            let number: u32 = match simulation_run_count_arg.parse() {
+                Ok(n) => n,
+                Err(_) => {
+                    eprintln!(
+                        "error: you must give the program a simulation count as the first argument"
+                    );
+                    return;
+                }
+            };
+            simulation_run_count = number;
+
+            let user_route_arg = &args[2];
+            println!("user_route_arg={}", user_route_arg);
+            let user_route: Vec<usize> = user_route_arg
+                .split(",")
+                .into_iter()
+                .map(|idx| idx.trim().parse::<usize>().unwrap())
+                .collect();
+            seed_route = user_route.clone();
+
+            println!("Parsed user route");
+        }
         _ => {
             // show a help message
             help();
@@ -61,10 +88,15 @@ fn main() {
 
     let json_string = get_json();
 
-    type Dictionary = HashMap<u32, StarSystem>;
+    type Dictionary = HashMap<usize, StarSystem>;
     let star_systems: Dictionary = serde_json::from_str(json_string).unwrap();
 
-    let star_system_keys = Vec::from_iter(star_systems.keys().sorted());
+    let star_system_keys: Vec<usize> = star_systems
+        .keys()
+        .sorted()
+        .into_iter()
+        .map(|x| *x)
+        .collect();
 
     // Dump each system in order, to validate
     for key in star_systems.keys().sorted() {
@@ -80,6 +112,21 @@ fn main() {
     let star_system_count = star_systems.keys().len();
     println!("Total system count: {}", star_system_count);
 
+    // validate the user route
+    let mut star_system_keys_validation_copy = star_system_keys.clone();
+    let mut seed_route_keys_validation_copy = seed_route.clone();
+    let seed_route_is_valid = eq_lists_inplace(
+        &mut star_system_keys_validation_copy,
+        &mut seed_route_keys_validation_copy,
+    );
+
+    if seed_route_is_valid {
+        println!("Provided route {:?} is valid", seed_route);
+    } else if seed_route.len() > 0 {
+        println!("Provided route {:?} is NOT valid", seed_route);
+        return;
+    }
+
     // 2d array of cached distances
     let mut star_system_distances_2d: Vec<Vec<f32>> =
         vec![vec![0.0; star_system_count]; star_system_count];
@@ -93,8 +140,8 @@ fn main() {
         let dest_pos = cgmath::vec3(dss.x, dss.y, dss.z);
         let distance = source_pos.distance(dest_pos);
 
-        star_system_distances_2d[**source_key as usize][**dest_key as usize] = distance;
-        star_system_distances_2d[**dest_key as usize][**source_key as usize] = distance;
+        star_system_distances_2d[*source_key as usize][*dest_key as usize] = distance;
+        star_system_distances_2d[*dest_key as usize][*source_key as usize] = distance;
     }
 
     println!(
@@ -106,17 +153,23 @@ fn main() {
     let mut overall_best_route_distance = std::f32::MAX;
     let mut overall_best_route: Vec<usize> = Vec::new();
 
-    let _initial_temperature = 600.0;
-    let _cooling_ratio = 0.997;
-    let iterations_at_temperature: u32 = 3000;
+    let _initial_temperature = 900.0;
+    let _cooling_ratio = 0.998;
+    let iterations_at_temperature: u32 = 2000;
     let mut total_iterations: u64 = 0;
 
-    // let mut rng = rand::thread_rng();
+    let mut rng = rand::thread_rng();
     let simulation_durations_count = 10;
     let mut simulation_durations_in_millis: VecDeque<u32> =
         VecDeque::with_capacity(simulation_durations_count);
 
     let mut last_simulation_iteration_count = 0;
+
+    let mut randomised_route: Vec<usize> = star_system_keys
+        .into_iter()
+        .map(|x| usize::try_from(x).unwrap())
+        .collect_vec();
+    randomised_route.shuffle(&mut rng);
 
     for i in 1..simulation_run_count {
         // let random_initial_temperature = rng.gen_range(500.0..700.0);
@@ -152,8 +205,14 @@ fn main() {
         }
         println!("{}", output_string);
 
+        let initital_route: Vec<usize> = if seed_route_is_valid {
+            seed_route.clone()
+        } else {
+            randomised_route.clone()
+        };
+
         let (simulation_best_route, iterations) = run_simulation(
-            &star_system_keys,
+            &initital_route,
             &star_system_distances_2d,
             _initial_temperature,
             iterations_at_temperature,
@@ -204,36 +263,28 @@ fn main() {
     );
 }
 
-fn print_named_route(route: &Vec<usize>, star_systems: &HashMap<&u32, String>) {
+fn print_named_route(route: &Vec<usize>, star_systems: &HashMap<&usize, String>) {
     for idx in 1..route.len() {
-        let source = route[idx - 1] as u32;
-        let dest = route[idx] as u32;
+        let source = route[idx - 1];
+        let dest = route[idx];
 
         println!("{} -> {}", star_systems[&source], star_systems[&dest]);
     }
 }
 
 fn run_simulation(
-    star_system_keys: &Vec<&u32>,
+    initial_route: &Vec<usize>,
     distance_cache: &Vec<Vec<f32>>,
     initial_temperature: f32,
     iterations_at_temperature: u32,
     cooling_ratio: f32,
 ) -> (Vec<usize>, u32) {
-    // We need to deref the keys in this map with * as we originally got pointers from the original parsed JSON
-    let mut current_route: Vec<usize> = star_system_keys
-        .into_iter()
-        .map(|x| usize::try_from(**x).unwrap())
-        .collect_vec();
-
-    // Initialise an RNG for the shuffles
     let mut rng = rand::thread_rng();
 
     let mut best_route: Vec<usize> = Vec::new();
     let mut best_route_distance = f32::MAX;
 
-    // shuffle once, to allow us to re-run the binary with a randomised outcome each time
-    current_route.shuffle(&mut rng);
+    let mut current_route = initial_route.clone();
 
     let mut temperature = initial_temperature;
     let mut loop_count: u32 = 0;
@@ -263,8 +314,8 @@ fn run_simulation(
             let new_route_distance = route_distance_2d_array(&new_route, distance_cache);
 
             let exponent_args = (best_route_distance - new_route_distance) / temperature;
-
             let accept_probability_f32 = E.powf(exponent_args);
+
             // let accept_probability = f64::exp(exponent_args.into());
             let random_value: f32 = rng.gen();
             if random_value < accept_probability_f32 {
@@ -525,4 +576,15 @@ struct StarSystem {
     x: f32,
     y: f32,
     z: f32,
+}
+
+// To compare lists of routes
+fn eq_lists_inplace<T>(a: &mut [T], b: &mut [T]) -> bool
+where
+    T: PartialEq + Ord,
+{
+    a.sort();
+    b.sort();
+
+    a == b
 }
